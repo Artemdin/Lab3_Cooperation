4043f80ae2646cce955f565cde15e636db561e67
Artemdin <140723327+Artemdin@users.noreply.github.com>
2025-04-07 18:51:41 +0300

    task_dinov (#3)
    * menu_update
    
    * working with tasks
    
    ---------
    
    Co-authored-by: IgnatenkoViktor <viktorignatenko11@gmail.com>

---
+++ Main/Palyony.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Palyony
+{
+	public static void ExecuteTask(int taskNumber)
+	{
+		switch (taskNumber)
+		{
+			case 1:
+				Task1();
+				break;
+			case 2:
+				Task2();
+				break;
+			default:
+				Console.WriteLine("Невідоме завдання.");
+				break;
+		}
+	}
+
+	private static void Task1()
+	{
+		Console.WriteLine("Виконується завдання 1 студента Пальонний Назар");
+
+		// Логіка для завдання 1(робота з масивами)
+	}
+
+	private static void Task2()
+	{
+		Console.WriteLine("Виконується завдання 2 студента Пальонний Назар");
+
+		// Логіка для завдання 2(зубчасті масиви)
+	}
+}
