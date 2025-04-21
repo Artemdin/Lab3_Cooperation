4c8a579f5e9e6b5d604a2f6a47eeb0e2c90346ea
Nazarqq1 <nazarpalonnij@gmail.com>
2025-04-21 17:35:16 +0300

    now deletes everything in 1 task

--- Main/Palionniy.cs
+++ Main/Palionniy.cs
@@ -27,8 +27,13 @@ public class Palionniy
         int k = CommonMethods.ReadInt("Введіть індекс K (починаючи з 0): ");
         int t = CommonMethods.ReadInt("Введіть кількість T: ");
 
-        if (k >= 0 && k + t <= arr.Length)
+        if (k >= 0 && k < arr.Length)
         {
+            if (k + t > arr.Length)
+            {
+                t = arr.Length - k;
+            }
+
             int[] newArr = new int[arr.Length - t];
             int j = 0;
             for (int i = 0; i < arr.Length; i++)
@@ -44,13 +49,44 @@ public class Palionniy
         }
         else
         {
-            Console.WriteLine("Неможливо видалити елементи. Перевірте індекс та кількість.");
+            Console.WriteLine("Неможливо видалити елементи. Перевірте індекс.");
         }
     }
 
 
     private static void Task2()
     {
-       
+        Console.WriteLine("Завдання 2: Видалити рядок з максимальним елементом");
+
+        int[][] arr = CommonMethods.InputJaggedArray();
+
+        int max = int.MinValue;
+        int maxRow = -1;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            foreach (int value in arr[i])
+            {
+                if (value > max)
+                {
+                    max = value;
+                    maxRow = i;
+                }
+            }
+        }
+
+        int[][] newArr = new int[arr.Length - 1][];
+        int index = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i != maxRow)
+            {
+                newArr[index++] = arr[i];
+            }
+        }
+
+        Console.WriteLine("Результат:");
+        CommonMethods.PrintJaggedArray(newArr);
     }
+
 }
